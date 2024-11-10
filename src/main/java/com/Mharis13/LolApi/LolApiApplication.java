94db0e203c857b4060a1015acf5967191f8b4764
Mharis13 <137278432+Mharis13@users.noreply.github.com>
2024-11-11 00:53:55 +0100

    restart

--- src/main/java/com/Mharis13/LolApi/LolApiApplication.java
+++ src/main/java/com/Mharis13/LolApi/LolApiApplication.java
@@ -3,12 +3,14 @@ package com.Mharis13.LolApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class LolApiApplication {
 
 	public static void main(String[] args) {
-	
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(LolApiApplication.class, args);
 	}
  
